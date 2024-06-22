c73e63f378f125ec93a5a2fcdb52583d8f23bc88
Gaurav <gauravmaharjan1@gmail.com>
2024-06-22 23:40:43 +0545

    Add Day 01 file

---
+++ 30_Days_Of_Code/Day01_Hello,World.js
@@ -0,0 +1,8 @@
+// print Hello World
+console.log("Hello, World!");
+// declaring the variable inputString that takes String as input
+var inputString = String(readLine());
+// function that prints the inputString
+function processData(inputString){
+    console.log(inputString);
+}
