82d57ffe5839afb0fcd1f1047026086311fbdcbb
Gaurav <gauravmaharjan1@gmail.com>
2024-07-03 23:28:05 +0545

    Add day09 coding file

---
+++ 30_Days_Of_Code/Day09-Recursion3.js
@@ -0,0 +1,17 @@
+//recursive method for calculating factorial
+function factorial(n) {
+    if(n<=1) return 1;
+    else return n*factorial(n-1);
+}
+
+function main() {
+    const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+
+    const n = parseInt(readLine().trim(), 10);
+
+    const result = factorial(n);
+
+    ws.write(result + '\n');
+
+    ws.end();
+}
