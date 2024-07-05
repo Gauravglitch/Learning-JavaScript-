ba20d8d428a712ad9849149afdc9b24664d59527
Gaurav <gauravmaharjan1@gmail.com>
2024-07-05 23:12:38 +0545

    Add day10 coding file

---
+++ 30_Days_Of_Code/Day10-Binary_Numbers.js
@@ -0,0 +1,16 @@
+
+function main() {
+    const n = parseInt(readLine().trim(), 10);
+let binary = n.toString(2); // convert decimal to binary
+let maxOnes = 0;
+let currentOnes = 0;
+for (let digit of binary) {
+  if (digit === '1') {
+    currentOnes++;
+    maxOnes = Math.max(maxOnes, currentOnes);
+  } else {
+    currentOnes = 0;
+  }
+}
+console.log(maxOnes);
+    }
