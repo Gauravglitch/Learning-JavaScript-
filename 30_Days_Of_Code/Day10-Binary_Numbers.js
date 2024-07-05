2ad47a4e24f35a28b8547cf97506286e7b7400a6
Gaurav <gauravmaharjan1@gmail.com>
2024-07-05 23:14:56 +0545

    Add comment to day10 file

--- 30_Days_Of_Code/Day10-Binary_Numbers.js
+++ 30_Days_Of_Code/Day10-Binary_Numbers.js
@@ -1,6 +1,6 @@
-
+// converts decimal number to binary and prints the count of no. of max ones in it
 function main() {
-    const n = parseInt(readLine().trim(), 10);
+    const n = parseInt(readLine().trim(), 10);//need to specify the radix base
 let binary = n.toString(2); // convert decimal to binary
 let maxOnes = 0;
 let currentOnes = 0;
