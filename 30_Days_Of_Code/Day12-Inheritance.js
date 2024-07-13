d183aca130c5aad13495c88762ad9e479e3f00cc
Gaurav <gauravmaharjan1@gmail.com>
2024-07-13 22:20:22 +0545

    Format the file and added comments

--- 30_Days_Of_Code/Day12-Inheritance.js
+++ 30_Days_Of_Code/Day12-Inheritance.js
@@ -5,50 +5,51 @@ class Person {
         this.lastName = lastName;
         this.idNumber = identification;
     }
-    
+
     printPerson() {
         console.log(
-            "Name: " + this.lastName + ", " + this.firstName 
+            "Name: " + this.lastName + ", " + this.firstName
             + "\nID: " + this.idNumber
         )
     }
 }//Student class inherits Person class 
 class Student extends Person {
     //constructor
-    constructor(firstName, lastName, idNumber, testScores){
+    constructor(firstName, lastName, idNumber, testScores) {
         //it calls the constructor of Person class
         super(firstName, lastName, idNumber)
-        
-        this.testScores = testScores}
-     //calculate function that returns a char after calculating the average of the testScores
-        calculate(){
-         let avg = 0
-         let sum = 0
-        for(let i=0; i<this.testScores.length; i++){
-             sum += this.testScores[i]
-             avg  = sum / this.testScores.length
+
+        this.testScores = testScores
+    }
+    //calculate function that returns a char after calculating the average of the testScores
+    calculate() {
+        let avg = 0
+        let sum = 0
+        for (let i = 0; i < this.testScores.length; i++) {
+            sum += this.testScores[i]
+            avg = sum / this.testScores.length
         }
-        
-        if(avg>=90 && avg <=100){
-                return 'O'
-                
-            }else if(avg>=80 && avg<90){
-                
-                return 'E'
-            }else if(avg>=70 && avg<80){
-                
-                return 'A'
-            }else if(avg>=55 && avg<70){
-                
-                 return 'P'
-            }else if(avg>=40 && avg<55){
-                
-                return 'D'
-            }else if(avg<40){
-                
-                return 'T'
-            }
-        
+
+        if (avg >= 90 && avg <= 100) {
+            return 'O'
+
+        } else if (avg >= 80 && avg < 90) {
+
+            return 'E'
+        } else if (avg >= 70 && avg < 80) {
+
+            return 'A'
+        } else if (avg >= 55 && avg < 70) {
+
+            return 'P'
+        } else if (avg >= 40 && avg < 55) {
+
+            return 'D'
+        } else if (avg < 40) {
+
+            return 'T'
+        }
+
     }
 }
 
@@ -58,9 +59,9 @@ function main() {
     let id = +read()
     let numScores = +read()
     let testScores = new Array(numScores)
-    
+
     for (var i = 0; i < numScores; i++) {
-        testScores[i] = +read()  
+        testScores[i] = +read()
     }
 
     let s = new Student(firstName, lastName, id, testScores)
